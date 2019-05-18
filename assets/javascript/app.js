016979bd4208e725e3a250ef611ef4889fea29df
keensteven <keenstevie@gmail.com>
2019-05-18 18:05:04 -0400

    visibility works

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+console.log ("works")
+
+var targetDiv = document.getElementById("visibility");
+
+targetDiv.setAttribute("class", "invisible");
+
+// In this example, we instead create a new div called "newDiv".
+// var newDiv = document.createElement("div");
+
+// // We then give this newDiv the text "A pleasure to meet you!".
+// // newDiv.textContent = "A pleasure to meet you!";
+
+// // // Now we use the ".appendChild" method to combine the two divs together on the page.
+// // targetDiv.appendChild(newDiv);
+// newDiv.
+
+// if (document.getElementById('q1').checked) {
+//     alert("gotit");
+//   }
