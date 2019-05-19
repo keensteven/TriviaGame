8d470095a07297ab5b9d3eaf0ee10aefa5420c7f
keensteven <keenstevie@gmail.com>
2019-05-19 17:44:18 -0400

    scroll to top working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,41 +8,27 @@ $(document).ready(function () {
 
     setTimeout(sixtySeconds, 1000 * 10);
 
-function sixtySeconds() {
-  // in the element with an id of time-left add an h2 saying Time's Up!
-  // console log done
-  var targetDiv = document.getElementById("visibility");
-  targetDiv.setAttribute("class", "invisible");
-  console.log("done");
+    function sixtySeconds() {
+      // in the element with an id of time-left add an h2 saying Time's Up!
+      // console log done
+      var targetDiv = document.getElementById("visibility");
+      targetDiv.setAttribute("class", "invisible");
+      console.log("done");
 
-  var scoreDiv = document.getElementById("scorevisible");
-  scoreDiv.setAttribute("class", "visible");
+      var scoreDiv = document.getElementById("scorevisible");
+      scoreDiv.setAttribute("class", "visible");
 
-  var buttonDiv = document.getElementById("buttonvisible");
-  buttonDiv.setAttribute("class", "invisible");
-  // $("#time-left").append("<h2>Time's Up!</h2>");
-  console.log("time is up");
-}
+      var buttonDiv = document.getElementById("buttonvisible");
+      buttonDiv.setAttribute("class", "invisible");
+      alert("Time is up")
+      $('html, body').animate({ scrollTop: 0 }, 'fast');
+      console.log("time is up");
+    }
   });
 
 });
 
-// var audio = new Audio("raven.mp3");
 
-// //  after 60 seconds, execute the tenSeconds function
-// //  after 15 seconds, execute the timeUp function
-
-// setTimeout(timeUp, 1000 * 15);
-
-// function sixtySeconds() {
-//   // in the element with an id of time-left add an h2 saying About 5 Seconds Left!
-//   // console log 5 seconds left
-//   $("#time-left").append("<h2>About 5 Seconds Left!</h2>");
-//   console.log("5 seconds left");
-// }
-
-//   //  The following line will play the audio file we linked to above:
-//   audio.play();
 
 
 /* 
