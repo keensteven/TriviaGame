ba24675afe9b0010c64c7b925ba29d471e5cffb8
keensteven <keenstevie@gmail.com>
2019-05-20 16:08:18 -0400

    complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,129 @@
-console.log("works")
+var correct = 0;
+var incorrect = 0;
+var unanswered = 5;
+
+$("#unanswered-score").text(unanswered);
+
+
+$("#q1").on("click", function () {
+  correct++
+  unanswered--
+  $("#correct-score").text(correct);
+  $("#incorrect-score").text(incorrect);
+  $("#unanswered-score").text(unanswered);
+})
+
+$("#q2").on("click", function () {
+  unanswered--
+  incorrect++
+  $("#correct-score").text(correct);
+  $("#incorrect-score").text(incorrect);
+  $("#unanswered-score").text(unanswered);
+})
+
+$("#q3").on("click", function () {
+  unanswered--
+  incorrect++
+  $("#correct-score").text(correct);
+  $("#incorrect-score").text(incorrect);
+  $("#unanswered-score").text(unanswered);
+})
+
+$("#q4").on("click", function () {
+  unanswered--
+  incorrect++
+  $("#correct-score").text(correct);
+  $("#incorrect-score").text(incorrect);
+  $("#unanswered-score").text(unanswered);
+})
+
+$("#q5").on("click", function () {
+  unanswered--
+  incorrect++
+  $("#correct-score").text(correct);
+  $("#incorrect-score").text(incorrect);
+  $("#unanswered-score").text(unanswered);
+})
+
+$("#q6").on("click", function () {
+  correct++
+  unanswered--
+  $("#correct-score").text(correct);
+  $("#incorrect-score").text(incorrect);
+  $("#unanswered-score").text(unanswered);
+})
+
+$("#q7").on("click", function () {
+  incorrect++
+  unanswered--
+  $("#correct-score").text(correct);
+  $("#incorrect-score").text(incorrect);
+  $("#unanswered-score").text(unanswered);
+})
+
+$("#q8").on("click", function () {
+  correct++
+  unanswered--
+  $("#correct-score").text(correct);
+  $("#incorrect-score").text(incorrect);
+  $("#unanswered-score").text(unanswered);
+})
+
+$("#q9").on("click", function () {
+  incorrect++
+  unanswered--
+  $("#correct-score").text(correct);
+  $("#incorrect-score").text(incorrect);
+  $("#unanswered-score").text(unanswered);
+})
+
+$("#q10").on("click", function () {
+  incorrect++
+  unanswered--
+  $("#correct-score").text(correct);
+  $("#incorrect-score").text(incorrect);
+  $("#unanswered-score").text(unanswered);
+})
+
+$("#q11").on("click", function () {
+  correct++
+  unanswered--
+  $("#correct-score").text(correct);
+  $("#incorrect-score").text(incorrect);
+  $("#unanswered-score").text(unanswered);
+})
+
+$("#q12").on("click", function () {
+  incorrect++
+  unanswered--
+  $("#correct-score").text(correct);
+  $("#incorrect-score").text(incorrect);
+  $("#unanswered-score").text(unanswered);
+})
+
+$("#q13").on("click", function () {
+  correct++
+  unanswered--
+  $("#correct-score").text(correct);
+  $("#incorrect-score").text(incorrect);
+  $("#unanswered-score").text(unanswered);
+})
+
+$("#q14").on("click", function () {
+  incorrect++
+  unanswered--
+  $("#correct-score").text(correct);
+  $("#incorrect-score").text(incorrect);
+  $("#unanswered-score").text(unanswered);
+})
+
+$("#q15").on("click", function () {
+  incorrect++
+  unanswered--
+  $("#correct-score").text(correct);
+  $("#incorrect-score").text(incorrect);
+  $("#unanswered-score").text(unanswered);
+})
 
 $(document).ready(function () {
 
@@ -6,9 +131,11 @@ $(document).ready(function () {
     var targetDiv = document.getElementById("visibility");
     targetDiv.setAttribute("class", "visible");
 
-    setTimeout(sixtySeconds, 1000 * 10);
 
-    function sixtySeconds() {
+
+    setTimeout(fortySeconds, 1000 * 40);
+
+    function fortySeconds() {
       // in the element with an id of time-left add an h2 saying Time's Up!
       // console log done
       var targetDiv = document.getElementById("visibility");
@@ -21,18 +148,14 @@ $(document).ready(function () {
       var buttonDiv = document.getElementById("buttonvisible");
       buttonDiv.setAttribute("class", "invisible");
       alert("Time is up")
-      $('html, body').animate({ scrollTop: 0 }, 'fast');
-      console.log("time is up");
+      $('html, body').animate({
+        scrollTop: 0
+      }, 'fast');
     }
   });
 
+
+
+
 });
 
-
-
-
-/* 
-        $(".answer").on("click",function(){
-            alert($(this).val())
-
-        }) */
